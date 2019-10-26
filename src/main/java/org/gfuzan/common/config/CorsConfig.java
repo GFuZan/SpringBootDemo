8f16dc0429ead41104d6894e4e17f724cc214d86
gfuzan <gfuzan@163.com>
2019-10-26 22:48:10 +0800

    设置Cors为web Bean

--- src/main/java/org/gfuzan/common/config/CorsConfig.java
+++ src/main/java/org/gfuzan/common/config/CorsConfig.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +22,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @author GFuZan
  */
 @Configuration
+@ConditionalOnWebApplication
 @ConditionalOnMissingBean(WebMvcConfigurer.class)
 public class CorsConfig {
 	
