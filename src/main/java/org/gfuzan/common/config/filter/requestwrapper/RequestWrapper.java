38bbee12517fb05b9a65e0cb2b6f05e5f00ce68b
gfuzan <gfuzan@163.com>
2019-11-17 20:50:44 +0800

    打印请求头

--- src/main/java/org/gfuzan/common/config/filter/requestwrapper/RequestWrapper.java
+++ src/main/java/org/gfuzan/common/config/filter/requestwrapper/RequestWrapper.java
@@ -2,12 +2,17 @@ package org.gfuzan.common.config.filter.requestwrapper;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.ReadListener;
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.util.StreamUtils;
 import org.springframework.util.StringUtils;
 
@@ -17,24 +22,64 @@ import org.springframework.util.StringUtils;
  */
 public class RequestWrapper extends HttpServletRequestWrapper {
 
-	private byte[] postBody;
+	private static final Logger logger = LoggerFactory.getLogger(RequestWrapper.class);
+
+	/**
+	 * 需要缓存的Mime列表
+	 */
+	private final static List<String> MIME_TYPES = Arrays.asList(MimeTypeUtils.APPLICATION_JSON_VALUE);
+
+	/**
+	 * 请求体缓存
+	 */
+	private final byte[] cacheBody;
+
+	/**
+	 * 是否需要缓存
+	 */
+	private final boolean isCache;
 
 	public RequestWrapper(HttpServletRequest request) {
 		super(request);
+
+		if (cacheJudge(this.getContentType())) {
+			byte[] body = null;
+			try {
+				body = StreamUtils.copyToByteArray(super.getInputStream());
+				logger.debug("缓存请求体. body:\n" + new String(body));
+			} catch (IOException e) {
+				logger.error("缓存请求体出错", e);
+			}
+			isCache = true;
+			cacheBody = body;
+		} else {
+			isCache = false;
+			cacheBody = null;
+		}
 	}
 
 	@Override
 	public ServletInputStream getInputStream() throws IOException {
 
-		if (StringUtils.hasLength(this.getContentType()) && this.getContentType().toLowerCase().indexOf("json") >= 0) {
-			if (postBody == null) {
-				postBody = StreamUtils.copyToByteArray(super.getInputStream());
-			}
-
-			return new CustomServletInputStream(postBody);
+		if (isCache) {
+			return new CustomServletInputStream(cacheBody);
 		}
 
 		return super.getInputStream();
+	}
+
+	/**
+	 * 缓存body判断
+	 * 
+	 * @param contentType
+	 * @return true 缓存,false 不缓存
+	 */
+	private boolean cacheJudge(String contentType) {
+		String mimeType = StringUtils.hasLength(contentType) ? contentType.split(";")[0] : null;
+		if (StringUtils.hasLength(mimeType) && MIME_TYPES.contains(mimeType)) {
+			return true;
+		}
+		return false;
 	}
 
 	/**
@@ -45,11 +90,11 @@ public class RequestWrapper extends HttpServletRequestWrapper {
 
 		private final ByteArrayInputStream inputStream;
 
-		CustomServletInputStream(byte[] postBody) {
-			if (postBody == null) {
-				postBody = new byte[0];
+		CustomServletInputStream(byte[] cacheBody) {
+			if (cacheBody == null) {
+				cacheBody = new byte[0];
 			}
-			this.inputStream = new ByteArrayInputStream(postBody);
+			this.inputStream = new ByteArrayInputStream(cacheBody);
 		}
 
 		@Override
--- src/main/java/org/gfuzan/common/config/filter/requestwrapper/RequestWrapperFilter.java
+++ src/main/java/org/gfuzan/common/config/filter/requestwrapper/RequestWrapperFilter.java
@@ -1,6 +1,7 @@
 package org.gfuzan.common.config.filter.requestwrapper;
 
 import java.io.IOException;
+import java.util.Enumeration;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -14,27 +15,45 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Reuqest替换Filter
+ * 
  * @author GFuZan
  *
  */
 public class RequestWrapperFilter implements Filter {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(RequestWrapperFilter.class);
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		if(request instanceof HttpServletRequest) {
+		if (request instanceof HttpServletRequest) {
 			HttpServletRequest requestWrapper = new RequestWrapper((HttpServletRequest) request);
 			request = requestWrapper;
-			
-			StringBuilder sb = new StringBuilder();
-			sb.append("RequestURL: '").append(requestWrapper.getRequestURI()).append("' ");
-			sb.append("ContentType: '").append(request.getContentType()).append("'");
-			logger.debug(sb.toString());
+
+			// 打印请求详情
+			{
+				StringBuilder sb = new StringBuilder();
+				sb.append("请求详情");
+
+				// 请求URL
+				sb.append('\n').append("RequestURL:");
+				sb.append('\n').append('\t');
+				sb.append(requestWrapper.getRequestURI());
+
+				// 请求头
+				sb.append('\n').append("RequestHead:");
+				Enumeration<String> headerNames = requestWrapper.getHeaderNames();
+				while (headerNames.hasMoreElements()) {
+					sb.append('\n').append('\t');
+					String headName = headerNames.nextElement();
+					sb.append(headName).append(':').append(requestWrapper.getHeader(headName));
+				}
+				logger.debug(sb.toString());
+			}
+
 		}
 		chain.doFilter(request, response);
-		
+
 	}
 
 }
